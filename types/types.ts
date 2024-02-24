46812305f767dae53082fd74d9581d3e959ed4a9
sbin <sbinha123@gmail.com>
2024-02-24 16:20:54 +0900

    wip: type 추가

--- types/types.ts
+++ types/types.ts
@@ -6,3 +6,30 @@ export type PostType = {
   updatedAt: Date;
   UserId: number;
 };
+
+export type UserType = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  website: string;
+  province: string;
+  city: string;
+  district: string;
+  street: string;
+  zipcode: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type CommentType = {
+  id: number;
+  content: string;
+  createdAt: Date;
+  updatedAt: Date;
+  UserId: number;
+  PostId: number;
+  User: Pick<UserType, 'name' | 'username' | 'email'>;
+  Post: PostType;
+};
